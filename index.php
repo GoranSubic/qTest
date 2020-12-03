20e25e016deb1eef6d9e23136c4841b7965fb448
Goran.Subic <gsubic@gmail.com>
2020-12-03 15:38:45 +0100

    Index page example links

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ require_once "bootstrap.php";
 
 echo "<h1>Students board</h1>";
 
+echo "<p><a href='?student=1'>Student 1 CSM board data</a></p>";
+echo "<p><a href='?student=2'>Student 2 CSM board data</a></p>";
+echo "<p><a href='?student=4'>Student 4 CSM board data</a></p>";
+
 if(isset($_GET['student'])) {
     $studentId = $_GET['student'];
 
